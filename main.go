639b86a1c45d7028abe9f5486c4beef23ae413a1
Quierounombre <vicengandrade@gmail.com>
2026-04-05 13:09:46 +0200

    getting yamls

--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
 import (
+	"github.com/goccy/go-yaml"
+	"os"
+	"fmt"
 )
 
-func main() {
+func get_file_content(name string) []byte {
+	var content		[]byte
+	var err			error
 
+	content, err = os.ReadFile(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return content
+}
+
+func extract_file_content(raw_yaml []byte) *Config {
+	var config		Config
+	var err			error
+
+	err = yaml.Unmarshal(raw_yaml, &config)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return (&config)
+}
+
+func get_config_from_file_name(name string) *Config {
+	var raw_yaml	[]byte
+	var config		*Config
+
+	raw_yaml = get_file_content(name)
+	config = extract_file_content(raw_yaml)
+	return (config)
+}
+
+func main() {
+	fmt.Printf("%+v\n", get_config_from_file_name("config.yaml"))
 }
